efaf399a3006c825dadd89701876dd76cf15af05
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-10-12 14:37:00 +0200

    Remove unnecessary code, add subpages components

---
+++ zaadoptuj-programiste/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import WelcomePage from './pages/WelcomePage'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+
+function App() {
+  return (
+    <>
+  <WelcomePage/>
+  <SignIn/>
+  <SignUp/>
+  <Projects/>
+</>
+  );
+}
+
+export default App;

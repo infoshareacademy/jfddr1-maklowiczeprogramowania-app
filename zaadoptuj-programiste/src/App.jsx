90b0f8311d09ef9ee6ea5550a40e254c78638243
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-10-13 22:17:04 +0200

    Add some small global css reset

--- zaadoptuj-programiste/src/App.jsx
+++ zaadoptuj-programiste/src/App.jsx
@@ -3,10 +3,21 @@ import WelcomePage from './pages/WelcomePage'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Quicksand', sans-serif;
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    color: #FBFBFB;
+  }
+`
 function App() {
   return (
     <>
+  <GlobalStyle/>
   <WelcomePage/>
   <SignIn/>
   <SignUp/>

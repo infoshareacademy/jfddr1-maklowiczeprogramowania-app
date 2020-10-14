317bae52917403f9f9c8c345f297f9d2f851f31b
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2020-10-14 14:29:05 +0200

    HamburgerMenu styles changed

--- zaadoptuj-programiste/src/components/HamburgerMenu.jsx
+++ zaadoptuj-programiste/src/components/HamburgerMenu.jsx
@@ -1,9 +1,10 @@
-
 import styled from 'styled-components'
 
 const HamburgerMenu = styled.img`
-    width:2em;
+position: absolute;
+right: 2rem;
+top: 2rem;
+width: 2.8rem;
 `
-
 export default HamburgerMenu
 

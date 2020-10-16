e036c59a5a8bb0a1ae731f1bb3a85afdf3fc074b
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2020-10-16 19:52:27 +0200

    Input component file formatting fixed

--- zaadoptuj-programiste/src/components/Input.jsx
+++ zaadoptuj-programiste/src/components/Input.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledInput = styled.input`
-    background-color: #2B2B3F;
-    color: #FBFBFB;
-    height: 50px;
-    padding: 10px;
-    border-radius: 5px;
-    border: none;
-    font-size: 1em;
+  background-color: #2b2b3f;
+  color: #fbfbfb;
+  height: 50px;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  font-size: 1em;
 `;
 
-const Input = ({type}) => {
-    return (
-        <StyledInput type={type}/>
-    );
-}
+const Input = ({ type }) => {
+  return <StyledInput type={type} />;
+};
 
 export default Input;

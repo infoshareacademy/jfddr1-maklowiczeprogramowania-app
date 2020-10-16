1e5fdd19cb4b328190a1801c40a3bcf94d4b0b62
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-10-16 22:46:55 +0200

    Add Wrapper component

---
+++ zaadoptuj-programiste/src/components/Wrapper.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.header`
+    width:100vw;
+    min-height:100vh;
+    padding: 0 2em;
+    background:#2B2B3F;
+    text-align:center;
+    overflow-x:hidden;
+`
+
+export default Wrapper
+

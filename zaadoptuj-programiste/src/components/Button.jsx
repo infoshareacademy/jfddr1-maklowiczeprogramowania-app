6beecc4f95cbdca5fd6d3c52eb4167da37a6f52b
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2020-10-16 18:45:17 +0200

    Button component styles fixed

--- zaadoptuj-programiste/src/components/Button.jsx
+++ zaadoptuj-programiste/src/components/Button.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledButton = styled.button`
-    background-color: #2B2B3F;
-    height: 80px;
-    border-radius: 5px;
-    border: none;
-    width: 70%;
-    color: #FBECCC;
-    font-size: 2rem;
+export const StyledButton = styled.button`
+  font-family: "Quicksand";
+  font-weight: 500;
+  background-color: #2b2b3f;
+  color: #fbeccc;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  font-size: 2rem;
 `;
 
-function Button({type, label}) {
-    return (
-        <>
-            <StyledButton type={type}> {label} </StyledButton>
-        </>
-    );
-}
+const Button = ({ type, label }) => {
+  return <StyledButton type={type}>{label}</StyledButton>;
+};
 
 export default Button;

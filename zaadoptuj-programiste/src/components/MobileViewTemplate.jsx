94b6ef98d2b2fc0150d7157544bb389abb321801
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2020-10-16 19:18:07 +0200

    MobTemplate file changed name to MobileViewTemplate, fixed some styles and formatting as well

---
+++ zaadoptuj-programiste/src/components/MobileViewTemplate.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+import HamburgerMenu from "./HamburgerMenu";
+import HamburgerMenuSrc from "../img/radix-icons_hamburger-menu-dark.svg";
+
+const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  background: #fbeccc;
+  color: #2b2b3f;
+  font-weight: 600;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const HamburgerMenuContainer = styled.section`
+  display: flex;
+  flex-grow: 1;
+  justify-content: flex-end;
+  align-items: flex-start;
+  margin: 30px 30px 0 0;
+`;
+
+const ChildrenContainer = styled.section`
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  align-self: center;
+  justify-self: center;
+  min-width: 80%;
+  max-width: 80%;
+`;
+
+const MobileViewTemplate = ({ children }) => {
+  return (
+    <MainContainer>
+      <HamburgerMenuContainer>
+        <HamburgerMenu src={HamburgerMenuSrc} />
+      </HamburgerMenuContainer>
+      <ChildrenContainer>
+        {children}
+        {/* {anotherNestedComponent} */}
+      </ChildrenContainer>
+    </MainContainer>
+  );
+};
+
+export default MobileViewTemplate;

4a6fc4c85d7cdb626738fccdb20b15f48d30b6f8
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2020-10-16 19:37:19 +0200

    SmallButton component created

---
+++ zaadoptuj-programiste/src/components/SmallButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import { StyledButton } from "../components/Button";
+
+const StyledSmallButton = styled(StyledButton)`
+  font-weight: 600;
+  font-size: 0.9rem;
+  padding: 17px 0 15px 0;
+  width: 50%;
+`;
+
+const SmallButton = ({ type, label }) => {
+  return <StyledSmallButton type={type}>{label}</StyledSmallButton>;
+};
+
+export default SmallButton;

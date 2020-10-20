3863ee7e93838bb0f602aff029c77d83cf6fb248
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2020-10-20 21:08:10 +0200

    MenuMobileTabs file added to mocks folder and used map method in MenuMobile file.

--- src/pages/MenuMobile.jsx
+++ src/pages/MenuMobile.jsx
@@ -1,28 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 import MobileViewTemplate from "../components/MobileViewTemplate";
+import { getMenuMobileTabsDB } from "../mocks/MenuMobileTabs.js";
 
-const MobileMenuAnchor = styled.a`
+const StyledMobileMenuTab = styled.a`
   padding: 15px;
   font-weight: 700;
   font-size: 2rem;
   text-align: center;
 `;
 
+const MobileMenuTab = ({ text }) => {
+  return <StyledMobileMenuTab>{text}</StyledMobileMenuTab>;
+};
+
 // const ExampleComp = () =>{
 //   return <div>Cześć</div>
 // }
 
 const MenuMobile = () => {
+  const searchedMenuMobileTabsDB = getMenuMobileTabsDB();
+  const searchedMenuMobileTabsComponents = searchedMenuMobileTabsDB.map(
+    ({ tabName }) => {
+      return <MobileMenuTab key={tabName} text={tabName} />;
+    }
+  );
+
   return (
     // <MobTemplate anotherNestedComponent={<ExampleComp/>} >
-    <MobileViewTemplate>
-      <MobileMenuAnchor>Zaloguj się</MobileMenuAnchor>
-      <MobileMenuAnchor>Zarejestruj się</MobileMenuAnchor>
-      <MobileMenuAnchor>Strona główna</MobileMenuAnchor>
-      <MobileMenuAnchor>Jak to działa?</MobileMenuAnchor>
-      <MobileMenuAnchor>Wiedza o IT</MobileMenuAnchor>
-    </MobileViewTemplate>
+    <MobileViewTemplate>{searchedMenuMobileTabsComponents}</MobileViewTemplate>
   );
 };
 

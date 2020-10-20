0f204f3f465a17462fb5501136e8cac0ff20a924
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2020-10-20 20:02:46 +0200

    AuthMenuMobile file added

---
+++ src/pages/AuthMenuMobile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import MobileViewTemplate from "../components/MobileViewTemplate";
+
+const MobileMenuAnchor = styled.a`
+  padding: 15px;
+  font-weight: 700;
+  font-size: 2rem;
+  text-align: center;
+`;
+
+const AuthMenuMobile = () => {
+  return (
+    <MobileViewTemplate>
+      <MobileMenuAnchor>Zaloguj się</MobileMenuAnchor>
+      <MobileMenuAnchor>Zarejestruj się</MobileMenuAnchor>
+      <MobileMenuAnchor>Strona główna</MobileMenuAnchor>
+      <MobileMenuAnchor>Jak to działa?</MobileMenuAnchor>
+      <MobileMenuAnchor>Wiedza o IT</MobileMenuAnchor>
+    </MobileViewTemplate>
+  );
+};
+
+export default AuthMenuMobile;

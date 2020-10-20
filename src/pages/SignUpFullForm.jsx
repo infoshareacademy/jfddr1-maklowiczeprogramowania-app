21984503a3af88f2560c9820ee922ae51c8d5cd3
Tomasz Belech <tomaszbelech1@gmail.com>
2020-10-20 11:54:04 +0200

    SignUpFullForm-Podstawowe-dane Wiew Done

--- src/pages/SignUpFullForm.jsx
+++ src/pages/SignUpFullForm.jsx
@@ -6,11 +6,27 @@ import Input from "../components/Input";
 import { SignInTitle } from "../components/SignIn.styled";
 import styled from "styled-components";
 
+const SlideItem = styled.span`
+  font-size: 2rem;
+`;
+
+const SlideItemActive = styled(SlideItem)`
+  color: #fbbb9a;
+`;
+
 const ButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   margin-top: 30px;
+  align-items: center;
+`;
+
+const SlideContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+  gap: 10px;
   align-items: center;
 `;
 
@@ -29,6 +45,12 @@ const SignUpFullForm = () => {
       <ButtonContainer>
         <SmallButton type={"submit"} label={"Dalej"} />
       </ButtonContainer>
+      <SlideContainer>
+        <SlideItemActive>•</SlideItemActive>
+        <SlideItem>•</SlideItem>
+        <SlideItem>•</SlideItem>
+        <SlideItem>•</SlideItem>
+      </SlideContainer>
     </MobileViewTemplate>
   );
 };

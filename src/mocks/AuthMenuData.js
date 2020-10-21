0060ec6fcc1beb5c99017913dc31f0ea909e8f2e
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-10-22 00:49:26 +0200

    Add AuthMenuData.js file and static data in it for auth menu

---
+++ src/mocks/AuthMenuData.js
@@ -0,0 +1,39 @@
+import search from "../img/search-icon-light.svg";
+import profile from "../img/profile-icon.svg";
+import notifications from "../img/bell-icon-light.svg";
+import messages from "../img/message-bar-icon-light.svg";
+import friends from "../img/friends-icon.svg";
+import projects from "../img/projects-icon.svg";
+import signOut from "../img/sign-out-icon.svg";
+
+export const authMenuDB = [
+  {
+    label: "Wyszukiwarka",
+    icon: search,
+  },
+
+  {
+    label: "Profil",
+    icon: profile,
+  },
+  {
+    label: "Powiadomienia",
+    icon: notifications,
+  },
+  {
+    label: "Wiadomości",
+    icon: messages,
+  },
+  {
+    label: "Znajomi",
+    icon: friends,
+  },
+  {
+    label: "Projekty",
+    icon: projects,
+  },
+  {
+    label: "Wyloguj się",
+    icon: signOut,
+  },
+];

d418f26582cee59f86ab09cb4c2af1d450c3bd36
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2020-10-23 12:56:42 +0200

    AuthMenuMobile - WIP: Used AuthNavigationTemplate component

--- src/pages/AuthMenuMobile.jsx
+++ src/pages/AuthMenuMobile.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import AuthNavigationTemplate from "../components/AuthNavigationTemplate";
 import HamburgerMenu from "../components/HamburgerMenu";
 import HamburgerMenuSrc from "../img/radix-icons_hamburger-menu-dark.svg";
-import { authMenuDB } from "../mocks/AuthMenuData.js";
-import { StyledSmallButton } from "../components/SmallButton";
 
 const HeaderContainer = styled.header`
   display: flex;
@@ -12,13 +11,11 @@ const HeaderContainer = styled.header`
   height: 22vh;
   background-color: var(--dark-clr);
   color: var(--light-clr);
-  // border: 2px solid red;
 `;
 
 const UserDetailsContainer = styled.section`
   display: flex;
   justify-content: center;
-  // border: 2px solid blue;
 `;
 
 const UserImageWrapper = styled.div`
@@ -26,11 +23,11 @@ const UserImageWrapper = styled.div`
 `;
 
 const UserImage = styled.img`
-  height: 60px;
-  width: 60px;
+  height: 50px;
+  width: 50px;
   background-color: var(--light-clr);
-  border-radius: 30px;
-  margin: 0 5px 0 10px;
+  border-radius: 25px;
+  margin: 0 7px 0 12px;
 `;
 
 const UserNameAndSpecWrapper = styled.div`
@@ -55,7 +52,6 @@ const TopIconsNavContainer = styled.section`
   background-color: var(--light-clr);
   padding: 10px 15px 10px 25px;
   gap: 20px;
-  // border: 2px solid green;
 `;
 
 const ReHamburgerMenu = styled(HamburgerMenu)`
@@ -65,84 +61,11 @@ const ReHamburgerMenu = styled(HamburgerMenu)`
 const MainContainer = styled.main`
   display: flex;
   flex-direction: column;
+  align-items: center;
   height: 88vh;
   background-color: var(--dark-clr);
   color: var(--light-clr);
-  // border: 2px solid orange;
 `;
-
-const AuthMenuMobileTabsContainer = styled.section`
-  display: flex;
-  flex-grow: 1;
-  flex-direction: column;
-  align-self: center;
-  justify-self: center;
-  min-width: 80%;
-  max-width: 80%;
-`;
-
-// const AuthMenuMobileTab = styled.a`
-//   padding: 22px 0 22px 50px;
-//   font-weight: 600;
-//   font-size: 1.2rem;
-// `;
-
-// const ReStyledMediumButton = styled(StyledButton)`
-//   background-color: var(--light-clr);
-//   color: var(--dark-clr);
-//   font-weight: 600;
-//   font-size: 1.2rem;
-//   margin-top: 22px;
-//   margin-left: 50px;
-//   padding: 20px 0;
-//   width: 15rem;
-// `;
-
-// const ReMediumButton = ({ type, label }) => {
-//   return <ReStyledMediumButton type={type}>{label}</ReStyledMediumButton>;
-// };
-
-const AuthMenuList = styled.ul`
-  justify-content: flex-start;
-  display: flex;
-  flex-direction: column;
-`;
-
-const AuthMenuItem = styled.li``;
-
-const AuthMenuOptionIcon = styled.img`
-  margin-right: 1.3em;
-`;
-
-const AuthMenuLink = styled.a`
-  margin-top: 2.3em;
-  font-weight: 600;
-  color: var(--light-clr);
-  display: inline-block;
-`;
-
-const CreateProjectButton = styled(StyledSmallButton)`
-  background-color: var(--light-clr);
-  color: var(--dark-clr);
-  padding: 1em 2em;
-  width: auto;
-  margin-top: 3em;
-`;
-
-const AuthNavigationTemplate = () => {
-  const authMenuData = authMenuDB;
-
-  const AuthMenuDataComponents = authMenuData.map(({ label, icon }) => {
-    return (
-      <AuthMenuItem key={label}>
-        <AuthMenuOptionIcon src={icon} />
-        <AuthMenuLink key={label}>{label}</AuthMenuLink>
-      </AuthMenuItem>
-    );
-  });
-
-  return <>{AuthMenuDataComponents}</>;
-};
 
 const AuthMenuMobile = () => {
   return (
@@ -164,12 +87,7 @@ const AuthMenuMobile = () => {
         </TopIconsNavContainer>
       </HeaderContainer>
       <MainContainer>
-        <AuthMenuMobileTabsContainer>
-          <AuthMenuList>
-            <AuthNavigationTemplate />
-            <CreateProjectButton>Stwórz projekt</CreateProjectButton>
-          </AuthMenuList>
-        </AuthMenuMobileTabsContainer>
+        <AuthNavigationTemplate />
       </MainContainer>
     </>
   );

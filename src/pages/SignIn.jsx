da02b906c0f63f80cf06887d3e7aae071534bcd5
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-10-23 04:23:50 +0200

    Add link to sign up desktop page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MobileViewTemplate from "../components/MobileViewTemplate";
+import { Link } from "react-router-dom";
 import Label from "../components/Label";
 import Input from "../components/Input";
 import Button from "../components/Button";
@@ -18,7 +19,10 @@ const SignIn = () => {
       <Label>{"Hasło"}</Label>
       <Input type={"password"} />
       <StyledParagraph>
-        Nie masz konta? Założ je <StyledAnchor href="/">tutaj</StyledAnchor>
+        Nie masz konta? Założ je{" "}
+        <Link to="../pages/SignUpDesktop">
+          <StyledAnchor>tutaj</StyledAnchor>
+        </Link>
       </StyledParagraph>
       <Button type={"submit"} label={"Zaloguj"} />
     </MobileViewTemplate>

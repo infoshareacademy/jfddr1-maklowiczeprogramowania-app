cf0a1459f001ddb9da197143159a1a18816494c7
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-10-23 04:22:28 +0200

    Remove unnecessary imports and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
-import AuthMenuMobile from "./pages/AuthMenuMobile";
-import AuthWelcomeView from "./pages/AuthWelcomeView";
-import MenuMobile from "./pages/MenuMobile";
-import Projects from "./pages/Projects";
-import SignIn from "./pages/SignIn";
-import SignUpDesktop from "./pages/SignUpDesktop";
-import SignUpFull1FormMobile from "./pages/signUpFull1Mobile";
-import SignUpFull6FormMobile from "./pages/signUpFull6Mobile";
-import SignUpMobile from "./pages/SignUpMobile";
-import WelcomePage from "./pages/WelcomePage";
-import AddProjectView0 from "./pages/AddProjectView0";
-import AddProjectView1 from "./pages/AddProjectView1";
-import AddProjectView2 from "./pages/AddProjectView2";
-import AddProjectView3 from "./pages/AddProjectView3";
-import AddProjectView4 from "./pages/AddProjectView4";
-import AddProjectViewDone from "./pages/AddProjectViewDone";
 import Router from "./Router";
 const GlobalStyle = createGlobalStyle`
   :root {
@@ -48,23 +32,6 @@ const App = () => {
     <>
       <GlobalStyle />
       <Router />
-      {/* <AddProjectView0 /> */}
-      {/* <AddProjectView1 /> */}
-      {/* <AddProjectView2 /> */}
-      {/* <AddProjectView3 /> */}
-      {/* <AddProjectView4 /> */}
-      {/* <AddProjectViewDone /> */}
-      {/* <AddProjectViewTemplate /> */}
-      {/* <AuthMenuMobile /> */}
-      {/* <AuthWelcomeView /> */}
-      {/* <MenuMobile /> */}
-      {/* <Projects /> */}
-      {/* <SignIn /> */}
-      {/* <SignUpDesktop /> */}
-      {/* <SignUpFull1FormMobile />
-      <SignUpFull6FormMobile /> */}
-      {/* <SignUpMobile /> */}
-      {/* <WelcomePage /> */}
     </>
   );
 };

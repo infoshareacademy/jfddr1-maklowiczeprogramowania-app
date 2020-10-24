6d053c0dbb48429b641141e35075f7dca8ff52bb
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2020-10-24 22:43:09 +0200

    Label StyledLabel component renamed to StyledLabelMobile and StyledLabelDesktop component added

--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-export const StyledLabel = styled.div`
+export const StyledLabelMobile = styled.div`
   margin: 25px 0 3px 0;
   display: inline-block;
   font-size: 1rem;
 `;
 
+export const StyledLabelDesktop = styled(StyledLabelMobile)`
+  margin: 45px 0 3px 0;
+`;
+
 const Label = ({ children }) => {
-  return <StyledLabel>{children}</StyledLabel>;
+  return <StyledLabelMobile>{children}</StyledLabelMobile>;
 };
 
 export default Label;

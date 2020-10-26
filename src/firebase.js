b1af92316cbd554b04f6bcd28a6047ae786b5317
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-10-26 22:59:18 +0100

    Fix typo

--- src/firebase.js
+++ src/firebase.js
@@ -10,5 +10,5 @@ const app = firebase.initializeApp({
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
-export const auth = (auth = app.auth());
+export const auth = app.auth();
 export default app;

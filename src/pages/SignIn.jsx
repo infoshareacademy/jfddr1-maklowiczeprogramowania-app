1eb331b1e738bfaff52f6e75abdeff9289a824cd
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-10-26 23:46:39 +0100

    Add logic needed to sign in user create & add Form component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
 import MediaQuery from "react-responsive";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Button from "../components/buttons/Button";
 import DesktopFormTemplate from "../components/templates/DesktopFormTemplate";
 import Input from "../components/Input";
@@ -21,23 +22,69 @@ const ButtonContainer = styled.div`
   margin: 15px 40px;
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ErrorMessageBackground = styled.div`
+  margin-top: 2em;
+  padding: 1em;
+  border-radius: 4px;
+  background: #fbbb9a;
+  color: #f49869;
+`;
+
+const ErrorMessageParagraph = styled.p`
+  font-size: 1.1rem;
+`;
+
 const SignInForm = () => {
+  const emailRef = React.createRef();
+  const passwordRef = React.createRef();
+  const [error, setError] = useState("");
+  const { signIn } = useAuth();
+  const history = useHistory();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    signIn(emailRef.current.value, passwordRef.current.value)
+      .then(() => {
+        history.push("/pages/AuthWelcomeView");
+      })
+      .catch(() => {
+        setError("Logowanie nie powiodło się, spróbuj jeszcze raz!");
+      });
+  };
+
   return (
     <>
       <SignInTitle>Zaloguj się</SignInTitle>
-      <StyledLabelDesktop htmlFor={"email"}>{"Email"}</StyledLabelDesktop>
-      <Input id={"email"} name={"email"} type={"email"} />
-      <StyledLabelDesktop htmlFor={"password"}>{"Hasło"}</StyledLabelDesktop>
-      <Input id={"password"} name={"password"} type={"password"} />
-      <StyledParagraph>
-        Nie masz konta? Założ je{" "}
-        <Link to="../pages/SignUpDesktop">
-          <StyledAnchor>tutaj</StyledAnchor>
-        </Link>
-      </StyledParagraph>
-      <ButtonContainer>
-        <Button type={"submit"} label={"Zaloguj"} />
-      </ButtonContainer>
+      <Form onSubmit={submitHandler}>
+        {error && (
+          <ErrorMessageBackground>
+            <ErrorMessageParagraph>{error}</ErrorMessageParagraph>
+          </ErrorMessageBackground>
+        )}
+        <StyledLabelDesktop htmlFor={"email"}>{"Email"}</StyledLabelDesktop>
+        <Input ref={emailRef} id={"email"} name={"email"} type={"email"} />
+        <StyledLabelDesktop htmlFor={"password"}>{"Hasło"}</StyledLabelDesktop>
+
+        <Input
+          ref={passwordRef}
+          id={"password"}
+          name={"password"}
+          type={"password"}
+        />
+        <StyledParagraph>
+          Nie masz konta? Założ je{" "}
+          <Link to="../pages/SignUpDesktop">
+            <StyledAnchor>tutaj</StyledAnchor>
+          </Link>
+        </StyledParagraph>
+        <ButtonContainer>
+          <Button type={"submit"} label={"Zaloguj"} />
+        </ButtonContainer>
+      </Form>
     </>
   );
 };

11e6680b6b71fbf0aa75bcd2d5097318940670f5
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2020-11-03 01:07:21 +0100

    MediumButton and SmallButton sizes fixed

--- src/components/buttons/SmallButton.jsx
+++ src/components/buttons/SmallButton.jsx
@@ -5,7 +5,7 @@ import { StyledButton } from "./Button";
 export const StyledSmallButton = styled(StyledButton)`
   font-weight: 600;
   font-size: 0.9rem;
-  padding: 1rem 0 1rem 0;
+  padding: 1rem 0;
   width: 50%;
 `;
 

b2c2782c69dc81921c922288a11f71e900a80e2f
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-11-04 01:53:06 +0100

    Add private routes for auth views

--- src/Router.jsx
+++ src/Router.jsx
@@ -34,24 +34,36 @@ const Router = () => {
         {/* <Route path="/pages/SignInDesktop">
           <SignInDesktop />
         </Route> */}
-        <Route path="/pages/AddProjectView0">
-          <AddProjectView0 />
-        </Route>
-        <Route path="/pages/AddProjectView1">
-          <AddProjectView1 />
-        </Route>
-        <Route path="/pages/AddProjectView2">
-          <AddProjectView2 />
-        </Route>
-        <Route path="/pages/AddProjectView3">
-          <AddProjectView3 />
-        </Route>
-        <Route path="/pages/AddProjectView4">
-          <AddProjectView4 />
-        </Route>
-        <Route path="/pages/AddProjectViewDone">
-          <AddProjectViewDone />
-        </Route>
+        <PrivateRouter
+          exact
+          path="/pages/AddProjectView0"
+          component={AddProjectView0}
+        ></PrivateRouter>
+        <PrivateRouter
+          exact
+          path="/pages/AddProjectView1"
+          component={AddProjectView1}
+        ></PrivateRouter>
+        <PrivateRouter
+          exact
+          path="/pages/AddProjectView2"
+          component={AddProjectView2}
+        ></PrivateRouter>
+        <PrivateRouter
+          exact
+          path="/pages/AddProjectView3"
+          component={AddProjectView3}
+        ></PrivateRouter>
+        <PrivateRouter
+          exact
+          path="/pages/AddProjectView4"
+          component={AddProjectView4}
+        ></PrivateRouter>
+        <PrivateRouter
+          exact
+          path="/pages/AddProjectViewDone"
+          component={AddProjectViewDone}
+        ></PrivateRouter>
         <PrivateRouter
           exact
           path="/pages/AuthWelcomeView"
@@ -91,7 +103,7 @@ const Router = () => {
           <UserProfileGuestView1Mobile />
         </Route>
         <Route path="/">
-          <SignUpFull3Mobile />
+          <WelcomePage />
         </Route>
       </Switch>
       <Route path="/pages/SignUpDesktop">

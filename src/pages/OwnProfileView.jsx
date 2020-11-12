a04f88eb295c94e4021438f9c327b02b49ce70d3
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-11-12 14:43:03 +0100

    Add OwnProfileView page

---
+++ src/pages/OwnProfileView.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import styled from "styled-components";
+import AuthDesktopTemplate from "./AuthDesktopTemplate";
+import { AuthDesktopMain } from "./AuthDesktopTemplate";
+import { useProject } from "../contexts/ProjectsContext";
+import { StyledButton } from "../components/buttons/Button";
+import ApplicationModal from "../components/ApplicationModal";
+
+const OwnProfile = () => {
+  const history = useHistory();
+  const { projectId } = useParams();
+  const { projectsState } = useProject();
+  const [showModal, setShowModal] = useState(false);
+
+  const ProjectTitle = styled.h1`
+    color: var(--dark-clr);
+    font-size: 2rem;
+  `;
+
+  return (
+    <AuthDesktopTemplate>
+      <AuthDesktopMain
+        children={
+          <>
+            <ProjectTitle>Twój profil</ProjectTitle>
+          </>
+        }
+      />
+    </AuthDesktopTemplate>
+  );
+};
+
+export default OwnProfile;

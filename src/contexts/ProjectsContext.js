d7c025036ab47342a563a2f91b8bf7cf140d3243
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-11-12 15:26:14 +0100

    Add firebase/state logic, projects are now fetching from firebase collection

--- src/contexts/ProjectsContext.js
+++ src/contexts/ProjectsContext.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-
+import { firebaseProjectsDB } from "../firebase/ProjectsDB";
 const ProjectContext = React.createContext();
 
 export const useProject = () => {
@@ -8,10 +8,34 @@ export const useProject = () => {
 
 export const ProjectsProvider = ({ children }) => {
   const [project, setProject] = useState([{}]);
+  const [initialState, setInitialState] = useState();
+  const [projectsState, setProjectsState] = useState();
+
+  useEffect(() => {
+    firebaseProjectsDB
+      .get()
+      .then((snapshot) => {
+        if (snapshot) {
+          const firebaseProjects = snapshot.docs.map((doc) => {
+            return doc.data();
+          });
+          setProjectsState(firebaseProjects);
+          setInitialState(firebaseProjects);
+        } else {
+          console.log("No such document!");
+        }
+      })
+      .catch(function (error) {
+        console.log("Error getting document:", error);
+      });
+  }, []);
 
   const value = {
     project,
     setProject,
+    setProjectsState,
+    projectsState,
+    initialState,
   };
   return (
     <>
